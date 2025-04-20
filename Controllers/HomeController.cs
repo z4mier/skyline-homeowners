0a3d6f2f15a0fab1597d35854ffaa7935d05b2f9
Zaira Mae Mier <z4mier@gmail.com>
2025-04-20 09:31:01 +0800

    With amenity page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace SkylineHOA.Controllers
         {
             return View();
         }
+        public IActionResult AmenityBooking()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
